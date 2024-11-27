8b3e69a82b4806e3bb88096a7e83e03faca55ea6
RizkyArdiansyahhh <rizky.ardiansyahhh06@gmail.com>
2024-11-28 04:13:38 +0700

    task 5 -> recursive

--- optional-05-recursive/fibonacci.js
+++ optional-05-recursive/fibonacci.js
@@ -1,5 +1,17 @@
 function fibonacci(n) {
+  const series = [];
 
+  for (let i = 0; i <= n; i++) {
+    if (i === 0) {
+      series.push(0);
+    } else if (i === 1) {
+      series.push(1);
+    } else {
+      series.push(series[i - 1] + series[i - 2]);
+    }
+  }
+
+  return series;
 }
 
 // Jangan hapus kode di bawah ini!
